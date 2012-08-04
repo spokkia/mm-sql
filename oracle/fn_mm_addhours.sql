a3e3894ca1bbee164a7b90361066fbf041d96f5c
Michele Marco <larthal@gmail.com>
2012-08-04 12:24:29 +0200

    Function for adding hours to a DATE

---
+++ oracle/fn_mm_addhours.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE FUNCTION fn_mm_addhours
+(
+  par_date    IN DATE,
+  par_hours   IN NUMBER
+)
+  RETURN DATE
+IS
+BEGIN
+  RETURN par_date + (par_hours / 24);
+END fn_mm_addhours;
+/
+
+CREATE OR REPLACE PUBLIC SYNONYM fn_mm_addhours FOR fn_mm_sequence;
+
+GRANT EXECUTE ON fn_mm_addhours TO PUBLIC;
