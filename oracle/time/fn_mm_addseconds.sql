1027ea3a8ec1e409772a34d88cb31ab18503a538
Michele Marco <larthal@gmail.com>
2015-08-13 21:07:24 +0200

    Refactoring struttura
    Creata sottocartella per gestione funzioni per date e ore

---
+++ oracle/time/fn_mm_addseconds.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE FUNCTION fn_mm_addseconds
+(
+  par_date      IN DATE,
+  par_seconds   IN NUMBER
+)
+  RETURN DATE
+IS
+BEGIN
+  RETURN par_date + (par_seconds / (24 * 60 * 60));
+END fn_mm_addseconds;
+/
+
+CREATE OR REPLACE PUBLIC SYNONYM fn_mm_addseconds FOR fn_mm_addseconds;
+
+GRANT EXECUTE ON fn_mm_addseconds TO PUBLIC;
+
+SELECT SYSDATE, fn_mm_addseconds (SYSDATE, 2) FROM DUAL;
